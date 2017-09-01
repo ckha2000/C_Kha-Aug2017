96b67c73ddf9b0f77e3dfb82a1919a248614a6cd
ckha2000 <christopher.kha2000@gmail.com>
2017-09-01 09:43:34 -0700

    calculate gpa

--- GPA/src/GPA.java
+++ GPA/src/GPA.java
@@ -1,13 +1,22 @@
 
 public class GPA {
 	public static void main(String[] args) {
-		double score = calcGPA(4.0, 3.5, 4.0);
-		System.out.println(score);
+		double grade1 = 4.0;
+		double grade2 = 3.0;
+		double grade3 = 2.0;
+		
+		double gpa = calcGPA(grade1, grade2, grade3);
+		printGPA(gpa);
 	}
 	
-	public static double calcGPA(double x, double y, double z) {
-		double sum = x + y + z;
+	public static void printGPA(double gpa) {
+		System.out.println(gpa);
+	}
+	
+	public static double calcGPA(double grade1, double grade2, double grade3) {
+		double sum = grade1 + grade2 + grade3;
 		double GPA = sum/3;
+		
 		return GPA;
 	}
 }
