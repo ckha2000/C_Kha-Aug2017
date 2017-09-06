7b34703072874c5b756749db53a3bb16046a4d9b
ckha2000 <christopher.kha2000@gmail.com>
2017-09-06 10:19:52 -0700

    Setup DoMath test class

---
+++ Access Calculate/src/DoMath.java
@@ -0,0 +1,13 @@
+/*Christopher Kha
+ *September 6, 2017 
+ *This is the runner for the Calculate Library.
+ *It is used to test the methods in Calculate.
+ */
+public class DoMath {
+
+	public static void main(String[] args) {
+		System.out.println(Calculate.square(-4));
+
+	}
+
+}
---
+++ Calculate Library/Calculate.java
@@ -0,0 +1,13 @@
+/*Christopher Kha
+ *September 6, 2017 
+ *Our self written math library. 
+ *It contains a series of methods to do basic math functions.
+ */
+public class Calculate {
+	
+	//square the input
+	public static int square(int operand) {
+		return operand*operand;
+	}
+	
+}
