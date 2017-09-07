7b9d5963350327dca4e45995b4910e3824b521a4
ckha2000 <christopher.kha2000@gmail.com>
2017-09-06 21:10:39 -0700

    Update DoMath.java

--- Access Calculate/src/DoMath.java
+++ Access Calculate/src/DoMath.java
@@ -7,7 +7,7 @@ public class DoMath {
 
 	public static void main(String[] args) {
 		
-		System.out.println(Calculate.toImproperFrac(1, 3, 4));
+		System.out.println(Calculate.foil(1, -4, 1, 4, "n")); 
 	}
 
 }
